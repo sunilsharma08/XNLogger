99f34ef27709dd2d1fb6ba4e9a77ab4b5200b442
Sunil Sharma <sunilsharma.ss08@gmail.com>
2019-02-12 22:20:05 +0530

    Added support for GZip request body for curl command

--- NetworkLogDemo/Pods/Target Support Files/Pods-NetworkLogDemo/Pods-NetworkLogDemo-frameworks.sh
+++ NetworkLogDemo/Pods/Target Support Files/Pods-NetworkLogDemo/Pods-NetworkLogDemo-frameworks.sh
@@ -143,9 +143,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/GzipSwift/Gzip.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NetworkLogger/NetworkLogger.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/GzipSwift/Gzip.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/NetworkLogger/NetworkLogger.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
