981f004dc7a1b098fd19c388beff8f67d1d10a58
Sunil Sharma <sunilsharma.ss08@gmail.com>
2019-03-09 11:36:29 +0530

    Updated file log handler

--- NetworkLogger/Logger/LogHandlers/NLFileLogHandler.swift
+++ NetworkLogger/Logger/LogHandlers/NLFileLogHandler.swift
@@ -11,11 +11,13 @@ import Foundation
 class NLFileLogHandler: NLBaseLogHandler, NLLogHandler {
     
     private let logComposer = LogComposer()
-    private(set) var fileName: String = "NLLog"
+    private(set) var fileName: String = "NLNetworkLog"
+    private let fileManager = FileManager.default
     
-    ///The max size a log file can be in Kilobytes. Default is 1024 (1 MB)
+    // The max size a log file can be in Kilobytes. Default is 1024 (1 MB)
     public var maxFileSize: UInt64 = 1024
-    //The max number of log file that will be stored. Once this point is reached, the oldest file is deleted.
+    
+    // The max number of log file that will be stored. Once this point is reached, the oldest file is deleted.
     public var maxFileCount = 4
     
     init(fileName: String?) {
@@ -33,7 +35,6 @@ class NLFileLogHandler: NLBaseLogHandler, NLLogHandler {
             if !fileManager.fileExists(atPath: directory) {
                 do {
                     try fileManager.createDirectory(atPath: directory, withIntermediateDirectories: true, attributes: nil)
-                    print(directory)
                 } catch let error {
                     debugPrint("Couldn't create directory at \(directory)")
                     debugPrint(error.localizedDescription)
@@ -42,21 +43,21 @@ class NLFileLogHandler: NLBaseLogHandler, NLLogHandler {
         }
     }
     
-    open var currentPath: String {
-        // TODO: Pick latest log instead of starting from 0th log file
+    private var currentPath: String {
         return "\(directory)/\(logName(0))"
     }
     
     // Get the default log directory
-    private class func defaultDirectory() -> String {
+    class func defaultDirectory() -> String {
         var path = ""
         let fileManager = FileManager.default
         let paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
         
         // Check atleast one path exists
         guard paths.count > 0
-            else { return path }
-        path = "\(paths[0])/NetworkLogs"
+        else { return path }
+        path = "\(paths[0])/NLNetworkLogs"
+        debugPrint("Log directory = \(path)")
         if !fileManager.fileExists(atPath: path) && !path.isEmpty  {
             do {
                 try fileManager.createDirectory(atPath: path, withIntermediateDirectories: false, attributes: nil)
@@ -69,8 +70,7 @@ class NLFileLogHandler: NLBaseLogHandler, NLLogHandler {
     
     // Check the size of a file
     func fileSize(_ path: String) -> UInt64 {
-        let fileManager = FileManager.default
-        let attrs: NSDictionary? = try? fileManager.attributesOfItem(atPath: path) as NSDictionary
+        let attrs: NSDictionary? = try? self.fileManager.attributesOfItem(atPath: path) as NSDictionary
         if let dict = attrs {
             return dict.fileSize()
         }
@@ -80,11 +80,11 @@ class NLFileLogHandler: NLBaseLogHandler, NLLogHandler {
     // Write content to the current log file.
     open func write(_ text: String) {
         let path = currentPath
-        let fileManager = FileManager.default
         if !fileManager.fileExists(atPath: path) {
             do {
                 try "".write(toFile: path, atomically: true, encoding: String.Encoding.utf8)
-            } catch _ {
+            } catch let error {
+                debugPrint(error.localizedDescription)
             }
         }
         if let fileHandle = FileHandle(forWritingAtPath: path) {
@@ -104,27 +104,30 @@ class NLFileLogHandler: NLBaseLogHandler, NLLogHandler {
         let maxSize: UInt64 = maxFileSize*1024
         if size > 0 && size >= maxSize && maxSize > 0 && maxFileCount > 0 {
             rename(0)
-            //delete the oldest file
+            // Delete the oldest file
             let deletePath = "\(directory)/\(logName(maxFileCount))"
             let fileManager = FileManager.default
             do {
                 try fileManager.removeItem(atPath: deletePath)
-            } catch _ {
+            } catch let error {
+                debugPrint("Failed to delete file \(deletePath)")
+                debugPrint(error.localizedDescription)
             }
         }
     }
     
     // Recursive method call to rename log files
     func rename(_ index: Int) {
-        let fileManager = FileManager.default
         let path = "\(directory)/\(logName(index))"
         let newPath = "\(directory)/\(logName(index+1))"
-        if fileManager.fileExists(atPath: newPath) {
+        if self.fileManager.fileExists(atPath: newPath) {
             rename(index+1)
         }
         do {
-            try fileManager.moveItem(atPath: path, toPath: newPath)
-        } catch _ {
+            try self.fileManager.moveItem(atPath: path, toPath: newPath)
+        } catch let error {
+            debugPrint("Failed to move file \(path) to path \(newPath)")
+            debugPrint(error.localizedDescription)
         }
     }
     
@@ -138,19 +141,18 @@ class NLFileLogHandler: NLBaseLogHandler, NLLogHandler {
     
     // Gets the log name
     func logName(_ num: Int) -> String {
-        return "\(fileName)-\(num).log"
+        return "\(self.fileName)-\(num).log"
     }
     
+    //MARK: Logging delegates
     public func logNetworkRequest(_ urlRequest: URLRequest) {
         if self.filters.count > 0 {
             for filter in filters where filter.shouldLog(urlRequest: urlRequest) {
-//                print(logComposer.getRequestLog(from: urlRequest))
                 write(logComposer.getRequestLog(from: urlRequest))
                 break
             }
         }
         else {
-//            print(logComposer.getRequestLog(from: urlRequest))
             write(logComposer.getRequestLog(from: urlRequest))
         }
     }
@@ -158,14 +160,11 @@ class NLFileLogHandler: NLBaseLogHandler, NLLogHandler {
     public func logNetworkResponse(for urlRequest: URLRequest, responseData: NLResponseData) {
         if self.filters.count > 0 {
             for filter in filters where filter.shouldLog(urlRequest: urlRequest) {
-//                print(logComposer.getResponseLog(urlRequest: urlRequest, response: responseData))
                 write(logComposer.getResponseLog(urlRequest: urlRequest, response: responseData))
                 break
             }
         } else {
             write(logComposer.getResponseLog(urlRequest: urlRequest, response: responseData))
-//            print(logComposer.getResponseLog(urlRequest: urlRequest, response: responseData))
         }
-        
     }
 }
