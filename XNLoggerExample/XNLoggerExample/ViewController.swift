e0d97b5dce150f0b7771944f3b58a81973b845c4
Sunil Sharma <sunilsharma.ss08@gmail.com>
2019-09-21 12:07:37 +0530

    Added example project

---
+++ XNLoggerExample/XNLoggerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XNLoggerExample
+//
+//  Created by Sunil Sharma on 21/09/19.
+//  Copyright © 2019 Sunil Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
