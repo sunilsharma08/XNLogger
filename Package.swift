c9c2ef753eb0c97f66aaaa7de089df7aa9fb608b
Sunil Sharma <sunilsharma.ss08@gmail.com>
2020-12-15 09:19:01 +0530

    SPM support fixes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -26,7 +26,7 @@ let package = Package(
             dependencies: [],
             path: "XNLogger",
             exclude: ["UI/XNLoader.m", "Info.plist", "XNLogger.h"],
-            sources: ["."]),
+            sources: ["XNLogger"]),
         .target(
             name: "XNLogger-ObjC",
             dependencies: ["XNLogger"],
