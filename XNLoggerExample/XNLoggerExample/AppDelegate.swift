04f0c078ad86d7d922c5f1f85d58b66087d6dd2a
Sunil Sharma <sunilsharma.ss08@gmail.com>
2026-08-28 00:06:39 +0530

    Fixed app crash issue

--- XNLoggerExample/XNLoggerExample/AppDelegate.swift
+++ XNLoggerExample/XNLoggerExample/AppDelegate.swift
@@ -11,11 +11,15 @@ import XNLogger
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var window: UIWindow?
-    
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
     //Optional XNLogger configuration
